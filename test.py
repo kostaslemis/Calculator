47a51c52c9fe7f0be1e341194796b83f2f205a50
kordo <kostasle@outlook.com>
2024-06-26 13:04:44 +0300

    Symbolic Differentiation

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
-import math
+import sympy
+from sympy.abc import x, y
 
-def func(x):
-    return math.exp(x)
+def func(x, y):
+    return sympy.exp(x) + y**3
 
-print(func(0.0))
+z = func(x, y)
+print('z =', z)
+
+dz_dx = z.diff(x)
+print('dz_dx =', dz_dx)
+
+dz_dy = z.diff(y)
+print('dz_dy =', dz_dy)
+print(dz_dy.evalf(subs={y: 1}))
